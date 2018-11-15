ae237ac83725faa090a100c336fecb1f16972afe
Julie Park <jspark721@gmail.com>
2018-11-14 16:24:20 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let defaultTextColor = '#FFFFFF';
+let defaultStyle = {
+  color: defaultTextColor
+}
+
+class Aggregate extends Component {
+  render() {
+    return(
+      <div style={{width: '40%', display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div>
+        <img/>
+        <input type="text" />
+        <span style={ defaultStyle }> Filter</span>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+          <li>Song 4</li>
+          <li>Song 5</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
+    let name = 'Julie'
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Playlist by {name}</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
       </div>
     );
   }
