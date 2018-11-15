64490bd645b6099b2fa0e8fb9ecf6f105d84db44
Julie Park <jspark721@gmail.com>
2018-11-15 11:54:29 -0800

    add playlist components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,75 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = '#FFFFFF';
 let defaultStyle = {
-  color: defaultTextColor
+  color: '#FFFFFF'
+};
+let fakeServerData = {
+  user: {
+    name: 'Julie',
+    playlists: [
+      {
+        name: 'My Favorites',
+        songs: [
+          { title:'Fire', duration: 13560 },
+          { title: 'Here', duration: 20000 },
+          { title: 'Walk Away', duration: 70000 },
+          { title: 'Mine', duration: 50431},
+          { title: 'Be Somebody', duration: 12048 }
+        ]
+      },
+      {
+        name: 'Top Hits',
+        songs: [
+          { title:'Happier', duration: 13560 },
+          { title: 'Taki Taki', duration: 20000 },
+          { title: 'thank u, next', duration: 70000},
+          { title: 'Without Me', duration: 50431}
+        ]
+      },
+      {
+        name: 'Chill Music',
+        songs: [
+          { title:'Stuck', duration: 13560 },
+          { title: 'The Reason', duration: 20000 },
+          { title: 'East Side', duration: 70000 },
+          { title: 'Sleepless Nights', duration: 50431},
+          { title: 'Location', duration: 12048 }
+        ]
+      },
+      {
+        name: 'Tropical Vibes',
+        songs: [
+          { title:'Fire', duration: 13560 },
+          { title: 'Here', duration: 20000 },
+          { title: 'Walk Away', duration: 70000 },
+          { title: 'Mine', duration: 50431 },
+          { title: 'Be Somebody', duration: 12048 }
+        ]
+      }
+    ]
+  }
 }
 
-class Aggregate extends Component {
+class PlaylistCounter extends Component {
   render() {
     return(
       <div style={{width: '40%', display: 'inline-block'}}>
-        <h2>Number Text</h2>
+        <h2>{ this.props.playlists.length } Playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, [])
+
+    return(
+      <div style={{width: '40%', display: 'inline-block'}}>
+        <h2>{ allSongs.length } Hours</h2>
       </div>
     );
   }
@@ -34,7 +93,7 @@ class Playlist extends Component {
     return(
       <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
         <img />
-        <h3>Playlist Name</h3>
+        <h3>Playlist</h3>
         <ul>
           <li>Song 1</li>
           <li>Song 2</li>
@@ -48,18 +107,34 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {}}
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData});
+    }, 1000);
+  }
   render() {
     let name = 'Julie'
     return (
       <div className="App">
-        <h1>Playlist by {name}</h1>
-        <Aggregate />
-        <Aggregate />
-        <Filter />
-        <Playlist />
-        <Playlist />
-        <Playlist />
-        <Playlist />
+        { this.state.serverData.user ?
+        <div>
+          <h1>Playlists by { this.state.serverData.user.name }</h1>
+
+          <PlaylistCounter playlists= { this.state.serverData.user.playlists }/>
+          <HoursCounter />
+
+          <Filter />
+
+          <Playlist />
+          <Playlist />
+          <Playlist />
+          <Playlist />
+        </div> : <h1>Loading...</h1>
+        }
       </div>
     );
   }
